50fb28b70dba52a38fa0c0db45821d241e35378d
faisalmerfin <faisalmerfin@gmail.com>
2019-07-21 12:11:33 +0700

    upload or external image_url

--- app/ProductImages.php
+++ app/ProductImages.php
@@ -10,4 +10,13 @@ class ProductImages extends Model
     {
         return $this->belongsTo('App\Product');
     }
+    
+    public function getImageUrlAttribute($value)
+    {
+        if (filter_var($value, FILTER_VALIDATE_URL)) {
+            return $value;
+        } else {
+            return asset('images/' . $value);
+        }
+    }
 }
--- app/Products.php
+++ app/Products.php
@@ -10,4 +10,13 @@ class Products extends Model
     {
         return $this->hasMany('App\ProductImages', 'product_id');
     }
+    
+    public function getImageUrlAttribute($value)
+    {
+        if (filter_var($value, FILTER_VALIDATE_URL)) {
+            return $value;
+        } else {
+            return asset('images/' . $value);
+        }
+    }
 }
