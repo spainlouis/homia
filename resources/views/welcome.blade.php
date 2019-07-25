db48f4d27ea4a7a26656e4d291c9b7677229e876
faisalmerfin <faisal.merfin@tokopedia.com>
2019-07-25 19:09:09 +0700

    segera hadir

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,18 +80,12 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravelsss
+                <div class="title m-b-md" style="color:orange;">
+                    Homia
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="links" style="color:orange;">
+                    Segera Hadir
                 </div>
             </div>
         </div>
